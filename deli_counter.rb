3c9a5a9e5e259bb0c1157b88f02f04702d26690b
KG ZZ <czheng5h88@gmail.com>
2019-09-09 11:01:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,21 +8,21 @@ def line(katz_deli)
     katz_deli.each_with_index{|val, index|
       res+= "#{index+1}. #{val}"
     }
-    return res 
+    puts res 
   end
 end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  return "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end 
 
 def now_serving(katz_deli)
   if katz_deli.length == 0 
-    return "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
     current = katz_deli.shift
-    return "Currently serving #{current}."
+    puts "Currently serving #{current}."
   end
 end
 
