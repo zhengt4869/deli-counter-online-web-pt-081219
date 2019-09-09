291e960bea03ca01603593873f484fbff15bb9a1
KG ZZ <czheng5h88@gmail.com>
2019-09-09 11:03:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else 
+    res = "The line is currently:"
+    katz_deli.each_with_index{|val, index|
+      res+= " #{index+1}. #{val}"
+    }
+    puts res 
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end 
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    current = katz_deli.shift
+    puts "Currently serving #{current}."
+  end
+end
+
+
